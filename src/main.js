b9535ca3ba761f6c3dc7ac24ca4f962db0a3302b
Wanqi-wq <1457208272@qq.com>
2020-02-08 00:10:26 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//引入fastclick
 import fastClick from 'fastclick'
+//引入vue-awesome-swiper
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
+
+
+
+Vue.use(VueAwesomeSwiper)
 
 
 
