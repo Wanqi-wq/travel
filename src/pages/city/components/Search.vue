32e0d7d50f78ab62c04aa75261868ed4261751f5
Wanqi-wq <1457208272@qq.com>
2020-02-11 00:02:38 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,65 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+    <input v-model="keyword" type="text" class="search-input" placeholder="输入城市名或拼音">
   </div>
+  <div class="search-content" ref="content" v-show="keyword">
+    <ul>
+      <div class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</div>
+      <div class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</div>
+    </ul>
+  </div>
+
+  </div>
+  
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: "CitySearch"
+  name: "CitySearch",
+  data() {
+    return {
+      keyword:'',
+      list:[],
+      timer: null
+    }    
+  },
+  computed:{
+    //判断list是否为空
+    hasNoData() {
+      return !this.list.length
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.content)
+  },
+  props: {
+    cities: Object
+  },
+  watch: {
+    keyword(keyword) {
+      if(this.timer) {
+        clearTimeout(this.timer)
+      }
+      if(!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+         for(let i in this.cities) {
+        this.cities[i].forEach(item => {
+          if(item.spell.includes(keyword) || item.name.includes(keyword)){
+            result.push(item)
+           }
+         })
+        }
+        this.list = result
+      },100)
+      
+    }
+  }
 }
 </script>
 
@@ -25,6 +78,20 @@ export default {
       padding .1rem
       border-radius .1rem
       color #666
+  .search-content
+    overflow hidden
+    position absolute
+    left 0
+    top 1.58rem
+    right 0
+    bottom 0
+    background #eee
+    z-index 1
+    .search-item 
+      line-height .62rem
+      padding-left .2rem
+      color #666
+      background #fff
 
       
 </style>
